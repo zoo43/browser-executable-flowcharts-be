164e098bf44a7e1b95b17b0f4d937ee8fb3f4bdd
Martz <matteo16.martini@gmail.com>
2024-06-15 15:57:22 +0200

    sdfa

--- server/exercises.py
+++ server/exercises.py
@@ -3,8 +3,8 @@ import json
 from flask import jsonify
 
 def getAll():
-    client = pymongo.MongoClient("mongodb://localhost:27017")
-    db = client["local"]
+    client = pymongo.MongoClient("mongodb+srv://matteomartini6:admin@cluster0.jpkdcgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client["Experiment-Data"]
     collection = db["Exercises"]
     res = collection.find()
     elements = []
